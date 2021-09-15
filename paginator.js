d62c092178a5050360d93eaeff880f490a397712
WILPKron <48627033+WILPKron@users.noreply.github.com>
2021-09-15 12:33:57 +0300

    Update paginator.js

--- paginator.js
+++ paginator.js
@@ -1,10 +1,9 @@
-function paginatorCreator({ totalPage: 1, currentPage: 1 }) {
+function paginatorCreator({ totalPage: 1, currentPage: 1, countIter: 2 }) {
    const paginatorLive = []
    if (totalPage > 1) {
      if (currentPage - 3 > 0) paginatorLive.push(1)
      if (currentPage - 4 === 1) paginatorLive.push(currentPage - 3)
      if (currentPage - 4 > 1) paginatorLive.push('...')
-     let countIter = 2
      if (currentPage < 5) countIter += 5 - currentPage
      if (totalPage - currentPage < 4) {
        countIter += 4 - (totalPage - currentPage)
